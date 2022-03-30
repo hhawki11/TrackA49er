b95754aadc32d6753c887956b8b0df90b4e50020
Michael Helwig <michaelhelwig2@gmail.com>
2022-03-30 16:39:39 -0400

    added popups when circle is clicked

--- tracka49er/src/HeatMap.js
+++ tracka49er/src/HeatMap.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { 
-MapContainer, 
-  TileLayer, 
-  Marker,
-  CircleMarker, 
-  Popup 
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    CircleMarker,
+    Popup
 } from "react-leaflet";
 import locData from "./GeoJson.json"
 
@@ -13,21 +13,24 @@ export default class HeatMap extends Component {
         return (
             <MapContainer center={[35.3075, -80.73371887207031]} zoom={17}>
                 <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
                 {locData.features.map(data => (
-                    <CircleMarker 
-                    key = {data.properties.device}
-                    color='black'
-                    radius = {data.properties.count * 5}
-                    weight={2}
-                    center={[data.geometry.coordinates[1],data.geometry.coordinates[0]]}>
+                    <CircleMarker
+                        key={data.properties.device}
+                        color='black'
+                        radius={data.properties.count * 5}
+                        weight={2}
+                        center={[data.geometry.coordinates[1], data.geometry.coordinates[0]]}>
+                        <Popup>
+                            {data.properties.device} <br/> {data.properties.count}
+                        </Popup>
                     </CircleMarker>
-                    
+
                 ))}
-                
+
             </MapContainer>
         )
     }
