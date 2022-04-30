c686ecb9d277d0573666c2dfc6339a21081cd83c
egomez4 <egomez4@uncc.edu>
2022-04-30 10:32:45 -0400

    high/low activity pages

--- tracka49er/src/highactivity.js
+++ tracka49er/src/highactivity.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import locdata from './GeoJson.json';
+
+const data = locdata.features; // replace this with the data you get from the get request
 
 function HighActivity() {
     return (
-        <h1> Places with lots of people: </h1>
+        <div className='plain-div'>
+            <h1> Active Places</h1>
+            <p> Here are some of the places on campus that currently have the highest activity. </p>
+
+            <div className='top-3-card'>
+                <div className='device'>
+                    {data[11].properties.device}
+                </div>
+                <div className='count'>
+                    {data[11].properties.count}
+                </div>
+                <p>Connections</p>
+            </div>
+
+            <div className='top-3-card'>
+
+                <div className='device'>
+                    {data[4].properties.device}
+                </div>
+
+                <div className='count'>
+                    {data[4].properties.count}
+                </div>
+
+                <p>Connections</p>
+
+            </div>
+
+            <div className='top-3-card'>
+                <div className='device'>
+                    {data[6].properties.device}
+                </div>
+                <div className='count'>
+                    {data[6].properties.count}
+                </div>
+                <p>Connections</p>
+            </div>
+        </div>
     );
 }
 
--- tracka49er/src/lowactivity.js
+++ tracka49er/src/lowactivity.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import locdata from './GeoJson.json';
+
+
+const data = locdata.features;
 
 function LowActivity() {
     return (
-        <h1> Best Places to Study:</h1>
+        <div className='plain-div'>
+            <h1> Best Places to Study</h1>
+            <p> Here are some of the places on campus that currently have the lowest activity. </p>
+
+            <div className='top-3-card'>
+                <div className='device'>
+                    {data[11].properties.device}
+                </div>
+                <div className='count'>
+                    {data[11].properties.count}
+                </div>
+                <p>Connections</p>
+            </div>
+
+            <div className='top-3-card'>
+
+                <div className='device'>
+                    {data[4].properties.device}
+                </div>
+
+                <div className='count'>
+                    {data[4].properties.count}
+                </div>
+
+                <p>Connections</p>
+
+            </div>
+
+            <div className='top-3-card'>
+                <div className='device'>
+                    {data[6].properties.device}
+                </div>
+                <div className='count'>
+                    {data[6].properties.count}
+                </div>
+                <p>Connections</p>
+            </div>
+        </div>
     );
+
 }
 
 export default LowActivity;
